3f3d8babd76aaf323484e0df0160d810e38ac3d6
VIVEK <90506249+Vivek212004@users.noreply.github.com>
2025-11-13 11:56:12 +0530

    Update 20251108060955_8b21d1e3-0588-43ff-8af5-ca8a56549609.sql

--- supabase/migrations/20251108060955_8b21d1e3-0588-43ff-8af5-ca8a56549609.sql
+++ supabase/migrations/20251108060955_8b21d1e3-0588-43ff-8af5-ca8a56549609.sql
@@ -6,3 +6,7 @@ COMMENT ON COLUMN products.is_sold_by_weight IS 'If true, base_price represents 
 
 ALTER TABLE products
 ADD COLUMN IF NOT EXISTS weight_tiers jsonb DEFAULT '[]'::jsonb;
+
+ALTER TABLE products
+   ADD COLUMN price numeric(10,2) DEFAULT 0 NOT NULL,
+   ADD COLUMN eggless boolean DEFAULT false NOT NULL;
