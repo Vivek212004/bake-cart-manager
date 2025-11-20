57ae8a646f505e939bd17652cdbdf7ca86da3af6
VIVEK <90506249+Vivek212004@users.noreply.github.com>
2025-11-20 11:57:10 +0530

    Add files via upload

---
+++ src/config/location.ts
@@ -0,0 +1,7 @@
+// src/config/location.ts
+export const BAKERY_LOCATION = {
+  lat: 17.44123, // 🔁 put your bakery latitude
+  lng: 78.39123, // 🔁 put your bakery longitude
+};
+
+export const MAX_DELIVERY_KM = 6;
