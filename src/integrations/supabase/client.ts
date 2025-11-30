06040800b2f3ebeb460e901716ec73b9863ea6c8
VIVEK <90506249+Vivek212004@users.noreply.github.com>
2025-11-30 12:49:08 +0530

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = "https://uzimddhgsfkjzdzexpjr.supabase.co";
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6aW1kZGhnc2ZranpkemV4cGpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM2NDA3MTAsImV4cCI6MjA3OTIxNjcxMH0.ZZCgokNBDWjt0UeoaFAl-sc9kuLQVveSgyl_meDVJuQ";
-
+const SUPABASE_SERVICE_ROLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6aW1kZGhnc2ZranpkemV4cGpyIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MzY0MDcxMCwiZXhwIjoyMDc5MjE2NzEwfQ.isKcwSQLbAcTBoCoU7SQ_Cb-SSykrerKGcjYWrEptdc"
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
